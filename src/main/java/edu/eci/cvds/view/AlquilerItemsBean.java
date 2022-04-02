f56af8dadba0f49e9e62f58aa9a0c2b1b253907f
laura valentina <jennyyana@hotmail.com>
2022-04-01 20:01:19 -0500

    lab 7 parte 3

--- src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
+++ src/main/java/edu/eci/cvds/view/AlquilerItemsBean.java
@@ -1,101 +1,73 @@
 package edu.eci.cvds.view;
+import javax.faces.bean.ManagedBean;
 
-import edu.eci.cvds.samples.entities.Item;
-import edu.eci.cvds.samples.entities.ItemRentado;
 import edu.eci.cvds.samples.entities.Cliente;
+import com.google.inject.Inject;
+
 import edu.eci.cvds.samples.services.ExcepcionServiciosAlquiler;
 import edu.eci.cvds.samples.services.ServiciosAlquiler;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.bean.ApplicationScoped;
-import javax.faces.bean.ManagedBean;
-import javax.faces.context.FacesContext;
-import javax.inject.Inject;
-
-import org.primefaces.model.SelectableDataModel;
-
-import java.util.List;
-
 import java.sql.Date;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import java.util.List;
+import javax.enterprise.context.ApplicationScoped;
 
-@ManagedBean(name = "AlquilerItemsBean")
+
+@ManagedBean(name="AlquilerItemsBean")
 @ApplicationScoped
-public class AlquilerItemsBean extends BasePageBean implements SelectableDataModel<Cliente> {
-    private List<Cliente> clientes = null;
-    private Cliente selectedCliente;
-
-    public AlquilerItemsBean() {
-    }
+public class AlquilerItemsBean extends BasePageBean{
 
     @Inject
     private ServiciosAlquiler serviciosAlquiler;
 
-    public List<Cliente> consultarClientes() {
+    private Cliente clienteSeleccionado;
+
+    public List<Cliente> consultarClientes(){
+        List<Cliente> clientes = null;
         try {
             clientes = serviciosAlquiler.consultarClientes();
+            return  clientes;
         } catch (ExcepcionServiciosAlquiler e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
         return clientes;
     }
+    /**
+     * Retorna el cliente que ha sido seleccionado.
+     * @return clienteSeleccionado.
+     */
+    public Cliente getClienteSeleccionado() {
+        return clienteSeleccionado;
+    }
+    /**
+     * Actualiza el cliente que ha sido seleccionado.
+     * @param clienteSeleccionado - nuevo cliente seleccionado.
+     */
+    public void setClienteSeleccionado(Cliente clienteSeleccionado) {
+        this.clienteSeleccionado = clienteSeleccionado;
+    }
 
-    public void registrarCliente(long doc, String nombre, String telefono, String direccion, String mail) {
+    /**
+     * Calcula la multa que tiene un cliente para un item específico.
+     * @param idItem - ID del item a calcular la multa
+     */
+    public long getMultaCliente(int idItem) {
+        long multa = 0;
         try {
-            Cliente cliente = new Cliente(nombre, doc, telefono, direccion, mail);
-            serviciosAlquiler.registrarCliente(cliente);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            multa = serviciosAlquiler.consultarMultaAlquiler(idItem, new Date(System.currentTimeMillis()));
+        } catch (ExcepcionServiciosAlquiler e) {
+            e.printStackTrace();
+        }
+        return multa;
+    }
+    /**
+     *
+     */
+    public void registrarCliente(String nombre, long documento, String telefono, String direccion, String email) {
+        Cliente clienteNuevo = new Cliente(nombre,documento,telefono,direccion,email);
+        try {
+            serviciosAlquiler.registrarCliente(clienteNuevo);
+        } catch (ExcepcionServiciosAlquiler e) {
+            e.printStackTrace();
         }
     }
-
-    public void setSelectedCliente(Cliente cliente) {
-        this.selectedCliente = cliente;
-    }
-
-    public Cliente getSelectedCliente() {
-        return selectedCliente;
-    }
-
-
-    public static java.util.Date parseDate(String date) {
-        try {
-            return new SimpleDateFormat("yyyy-MM-dd").parse(date);
-        } catch (ParseException e) {
-            return null;
-        }
-    }
-
-    public long consultarMultaAlquiler(int iditem) throws ExcepcionServiciosAlquiler {
-        try {
-            return serviciosAlquiler.consultarMultaAlquiler(iditem, new Date(System.currentTimeMillis()));
-        } catch (ExcepcionServiciosAlquiler ex) {
-            throw new ExcepcionServiciosAlquiler( "Error al consultar multa de item " + iditem);
-        }
-
-    }
-
-
-    @Override
-    public Object getRowKey(Cliente object) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public Cliente getRowData(String rowKey) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    /*Cambiar a pagina de alquiler*/
-    public String moveToPageRegistroAlquiler() {
-        return "registroAlquilercliente";
-    }
-    /*Cambiar a pagina de clientes*/
-    public String moveToPageRegistroCliente() {
-        return "registrocliente";
-    }
-
 }
